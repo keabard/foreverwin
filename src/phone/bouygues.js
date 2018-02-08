4f7e766b881ea42ea8cda7cd98e0ff703f9501ce
tsimonnet@xebia.fr <tsimonnet@xebia.fr>
2018-02-08 15:02:22 +0100

    Fix bouygues script with their deviant website

--- src/phone/bouygues.js
+++ src/phone/bouygues.js
@@ -5,26 +5,27 @@ import config from '../../config.json';
 const getBouyguesTelephone = async () => {
 
   await t
-    .navigateTo('https://www.bouyguestelecom.fr')
-    .click(Selector('article#js-hc--compte'))
-    .click(Selector('a').withText('Accéder à votre compte'))
+    .maximizeWindow()
+    .navigateTo('https://www.bouyguestelecom.fr/mon-compte')
+    // .click(Selector('article#js-hc--compte'))
+    // .click(Selector('a').withText('Accéder à votre compte'))
     .typeText(Selector('input#username'), config.phone.bouygues.login)
-    .typeText(Selector('input#password'), config.phone.bouygues.password)
-    .click(Selector('input#bt_valider'))
-    .wait(10000)
-    .click(Selector('a[data-link="invoices"]'));
+    .typeText(Selector('input#password'), config.phone.bouygues.password);
 
-  // if (await Selector('input#lastname').exists) {
-  //   await t.typeText(Selector('input#lastname'), config.phone.bouygues.lastName);
-  // }
-  //
-  // await t
-  //   .click(Selector('input#bt_valider'))
-  //   .debug()
-  //   .click(Selector('a[data-link="invoices"]'))
-  //   .click(Selector('a').withText('Télécharger'))
-  //   .debug()
-  //   .wait(5000);
+
+  if (await Selector('input#lastname').exists) {
+    await t.typeText(Selector('input#lastname'), config.phone.bouygues.lastName);
+  }
+
+  await t
+    .click(Selector('input#bt_valider'))
+
+  await Selector('div#dashboardTop')();
+
+  await t
+    .navigateTo('https://www.bouyguestelecom.fr/mon-compte/mes-factures')
+    .click(Selector('a').withText('Télécharger'))
+    .wait(5000);
 
   t.fixtureCtx.numericable = {
     price : 'coucou',
